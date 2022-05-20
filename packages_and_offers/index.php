6a5d99ea90931854882e13c001453107d0785241
Md Mushfikur Talukdar <mithutalukdar09@gmail.com>
2022-05-20 20:31:20 +0600

    Create index.php

---
+++ packages_and_offers/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+<h1 style="display:flex;justify-content: center;align-items: center; text-shadow: 0 0 10px white;">You Can Choose Your Desired Package From The Available Packages</h1>
+<div class="container">
+	
+	<div class="card">
+  <div class="card-image"></div>
+  <div class="card-text">
+    
+    <h2>Round Trip</h2>
+    
+  </div>
+  <div class="card-stats">
+    
+    <button id="btn">Buy</button>
+
+  </div>
+</div>
+
+<div class="card">
+  <div class="card-image"></div>
+  <div class="card-text">
+   
+    <h2>Whole day Journey With Food</h2>
+   
+  </div>
+  <div class="card-stats">
+    <button id="btn">Buy</button>
+  </div>
+</div>
+
+<div class="card">
+  <div class="card-image"></div>
+  <div class="card-text">
+    
+    <h2>Journey With Safety Guard</h2>
+  
+  </div>
+  <div class="card-stats">
+    <button id="btn">Buy</button>
+  </div>
+</div>
+</div>
+</body>
+</html>
+
+
+
+
