a058e54d3392c9a943133212a1bcf33eeed29cd1
Md Mushfikur Talukdar <mithutalukdar09@gmail.com>
2022-05-22 20:27:06 +0600

    Update guide.php

--- guide.php
+++ guide.php
@@ -1,11 +1,30 @@
-<?php
-include_once("connection.php");
+<style>
+.loader {
+  border: 16px solid #f3f3f3;
+  border-radius: 50%;
+  border-top: 16px solid #666362;
+  width: 120px;
+  height: 120px;
+  -webkit-animation: spin 2s linear infinite; /* Safari */
+  animation: spin 2s linear infinite;
+}
 
-$information= $_POST['Infor'];
-echo $information;
-    
-        $sql="INSERT INTO guidetable (Want guide) VALUES ('$information')";
+/* Safari */
+@-webkit-keyframes spin {
+  0% { -webkit-transform: rotate(0deg); }
+  100% { -webkit-transform: rotate(360deg); }
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+</style>
 
 
- ?>
-<h1>database is connect</h1>
+
+<div style="text-align : center; margin-top : 90px; color: #666362;">
+<h1>Please wait working on payment system</h1>
+</div>
+
+<div style="margin-top: 5cm;margin-left: 18cm;" class="loader"></div>
